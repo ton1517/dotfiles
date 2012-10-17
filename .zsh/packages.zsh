6eed5f16bf7d6fdc8ff728c514e189c2b8027433
ton <tonton1517@gmail.com>
2012-10-18 08:42:20 +0900

    add zsh package's config file.

---
+++ .zsh/packages.zsh
@@ -0,0 +1,39 @@
+#--------------------------------------------------------------------------------
+# plugin settings
+#--------------------------------------------------------------------------------
+
+source ~/.zsh/packages/package.zsh/package.zsh
+
+#----------------------------------------
+# zsh-completions
+#----------------------------------------
+
+package-install github zsh-users/zsh-completions
+fpath=($(package-directory zsh-users/zsh-completions)/src $fpath)
+
+#----------------------------------------
+# cdd
+#----------------------------------------
+
+package-install github m4i/cdd
+source $(package-directory m4i/cdd)/cdd
+
+#----------------------------------------
+# z
+#----------------------------------------
+
+package-install github rupa/z
+source $(package-directory rupa/z)/z.sh
+
+precmd() {
+      _z --add "$(pwd -P)"
+}
+
+#----------------------------------------
+# zaw
+#----------------------------------------
+
+package-install github nakamuray/zaw
+source $(package-directory nakamuray/zaw)/zaw.zsh
+
+bindkey '^R' zaw-history
