f35bd609206b4c1b5530fce9f4ff10fc954a86e4
ton <tonton1517@gmail.com>
2013-11-23 05:39:31 +0900

    Revert "unified packages.zsh and zshrc."
    This reverts commit 9cbb01fd177425a1544276fa5033d0708567aa66.

---
+++ .zsh/packages.zsh
@@ -0,0 +1,48 @@
+#--------------------------------------------------------------------------------
+# plugin settings
+#--------------------------------------------------------------------------------
+ autoload -Uz compinit
+ compinit
+
+source ~/.zsh/packages/package.zsh/package.zsh
+
+#----------------------------------------
+# zsh-completions
+#----------------------------------------
+
+package-install github zsh-users/zsh-completions
+fpath=($(package-directory zsh-users/zsh-completions)/src $fpath)
+
+#----------------------------------------
+# cdd
+#----------------------------------------
+
+package-install github m4i/cdd
+. $(package-directory m4i/cdd)/cdd
+
+#----------------------------------------
+# z
+#----------------------------------------
+
+package-install github rupa/z
+source $(package-directory rupa/z)/z.sh
+
+precmd() {
+      _z --add "$(pwd -P)"
+}
+
+#----------------------------------------
+# zaw
+#----------------------------------------
+
+package-install github nakamuray/zaw
+source $(package-directory nakamuray/zaw)/zaw.zsh
+
+bindkey '^R' zaw-history
+
+#----------------------------------------
+# zsh-syntax-highlighting
+#----------------------------------------
+
+package-install github zsh-users/zsh-syntax-highlighting
+source $(package-directory zsh-users/zsh-syntax-highlighting)/zsh-syntax-highlighting.zsh
