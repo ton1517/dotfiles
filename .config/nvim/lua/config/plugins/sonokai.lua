f0ade41f50234e03aeab6d8a1e8bb9cfedc6297a
ton <tonton1517@gmail.com>
2022-09-17 17:44:57 +0900

    set colorscheme after sonokai loaded.

--- .config/nvim/lua/config/plugins/sonokai.lua
+++ .config/nvim/lua/config/plugins/sonokai.lua
@@ -10,6 +10,8 @@ M.config = function()
   vim.g.sonokai_diagnostic_virtual_text = 'colored'
   vim.g.sonokai_current_word = 'underline'
   vim.g.sonokai_better_performance = 1
+
+  vim.cmd([[colorscheme sonokai]])
 end
 
 return M
