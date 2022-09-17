fa339345fa4438c1b2bd9ef667d2da64bd54508f
ton <tonton1517@gmail.com>
2022-09-17 17:09:57 +0900

    update sonokai option.

--- .config/nvim/lua/config/plugins/sonokai.lua
+++ .config/nvim/lua/config/plugins/sonokai.lua
@@ -8,6 +8,7 @@ M.config = function()
   vim.g.sonokai_disable_italic_comment = 1
   vim.g.sonokai_diagnostic_text_highlight = 1
   vim.g.sonokai_diagnostic_virtual_text = 'colored'
+  vim.g.sonokai_current_word = 'underline'
   vim.g.sonokai_better_performance = 1
 end
 
