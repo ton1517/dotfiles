5889a021107b0b3b3c96a1defcfa17f757cfdaff
ton <tonton1517@gmail.com>
2022-11-07 01:25:13 +0900

    asdf plugin add rust.

--- install-asdf-plugins.sh
+++ install-asdf-plugins.sh
@@ -6,5 +6,6 @@ asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin-add python https://github.com/asdf-community/asdf-python
 asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
 asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
+asdf plugin-add rust https://github.com/asdf-community/asdf-rust
 
 asdf install
