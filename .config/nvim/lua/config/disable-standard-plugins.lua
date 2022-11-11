d54ca9886fc039b3cfbec147fc196c395532c2c0
ton <tonton1517@gmail.com>
2022-11-11 11:54:47 +0900

    delete "loaded_netrw = 1"

--- .config/nvim/lua/config/disable-standard-plugins.lua
+++ .config/nvim/lua/config/disable-standard-plugins.lua
@@ -7,7 +7,6 @@ vim.g.loaded_zipPlugin = 1
 vim.g.loaded_man = 1
 vim.g.loaded_matchit = 1
 vim.g.loaded_matchparen = 1
-vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
 vim.g.loaded_netrwFileHandlers = 1
