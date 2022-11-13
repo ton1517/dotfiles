999140aa71d609215b87b0c4f030165ffeb5b223
ton <tonton1517@gmail.com>
2022-11-13 13:03:17 +0900

    use native function.

--- .config/nvim/lua/config/plugins/nvim-scrollbar.lua
+++ .config/nvim/lua/config/plugins/nvim-scrollbar.lua
@@ -4,6 +4,7 @@ M.setup = function() end
 
 M.config = function()
 	require("scrollbar.handlers.search").setup()
+	require("scrollbar.handlers.gitsigns").setup()
 	require("scrollbar").setup({
 		show = true,
 		set_highlights = true,
@@ -19,60 +20,23 @@ M.config = function()
 			Misc = { color = "#9d7cd8" },
 			GitAdd = {
 				color = "#9ed072",
-				text = { "+" },
-				priority = 5,
-				cterm = nil,
-				highlight = "CursorColumn",
+				text = "+",
 			},
 			GitDelete = {
 				color = "#fc5d7c",
-				text = { "-" },
-				priority = 5,
-				cterm = nil,
-				highlight = "CursorColumn",
+				text = "-",
 			},
 			GitChange = {
 				color = "#76cce0",
-				text = { "*" },
-				priority = 5,
-				cterm = nil,
-				highlight = "CursorColumn",
+				text = "*",
 			},
 		},
 		handlers = {
 			diagnostic = true,
 			search = true,
+			gitsigns = true,
 		},
 	})
-
-	local gitsign = require("gitsigns")
-	local gitsign_hunks = require("gitsigns.hunks")
-
-	require("scrollbar.handlers").register("git", function(bufnr)
-		local nb_lines = vim.api.nvim_buf_line_count(bufnr)
-		local colors_type = {
-			add = "GitAdd",
-			delete = "GitDelete",
-			change = "GitChange",
-			changedelete = "GitChange",
-		}
-
-		local lines = {}
-		local hunks = gitsign.get_hunks(bufnr)
-		if hunks then
-			for _, hunk in ipairs(hunks) do
-				hunk.vend = math.min(hunk.added.start, hunk.removed.start) + hunk.added.count + hunk.removed.count
-				local signs = gitsign_hunks.calc_signs(hunk, 0, nb_lines)
-				for _, sign in ipairs(signs) do
-					table.insert(lines, {
-						line = sign.lnum,
-						type = colors_type[sign.type],
-					})
-				end
-			end
-		end
-		return lines
-	end)
 end
 
 return M
