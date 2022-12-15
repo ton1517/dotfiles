3ee153ddc10877685d7149fa157c90d3b85e52e2
ton <tonton1517@gmail.com>
2022-12-15 13:13:13 +0900

    update lsp_signature.

--- .config/nvim/lua/config/plugins/lsp_signature.lua
+++ .config/nvim/lua/config/plugins/lsp_signature.lua
@@ -3,7 +3,11 @@ local M = {}
 M.setup = function() end
 
 M.config = function()
-	require("lsp_signature").setup()
+	require("lsp_signature").setup({
+		floating_window = false,
+		toggle_key = "<C-k>",
+		hint_prefix = " ",
+	})
 end
 
 return M
