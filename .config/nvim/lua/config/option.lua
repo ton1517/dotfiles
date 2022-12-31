646316c10e1cd036057f1591a82ec3b08dc879c4
ton <tonton1517@gmail.com>
2022-12-31 19:33:07 +0900

    avoid lsp warning.

--- .config/nvim/lua/config/option.lua
+++ .config/nvim/lua/config/option.lua
@@ -1,50 +1,49 @@
 local fn = vim.fn
-local opt = vim.opt
 local g = vim.g
 
-opt.laststatus = 3
-opt.updatetime = 100
-opt.shortmess:append({ c = true })
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.expandtab = true
-opt.cindent = true
-opt.showmatch = true
-opt.formatoptions = "tcqmMro"
-opt.visualbell = true
-opt.number = true
-opt.list = true
-opt.listchars = "tab:|-,extends:⇢,precedes:⇠,trail:»"
-opt.cmdheight = 0
-opt.title = true
-opt.pumheight = 20
-opt.signcolumn = "yes"
-opt.cursorline = true
-opt.fileformats = "unix,dos,mac"
-opt.mouse = "a"
-opt.ignorecase = true
-opt.smartcase = true
-opt.inccommand = "split"
-opt.breakindent = true
+vim.opt.laststatus = 3
+vim.opt.updatetime = 100
+vim.opt.shortmess:append({ c = true })
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.cindent = true
+vim.opt.showmatch = true
+vim.opt.formatoptions = "tcqmMro"
+vim.opt.visualbell = true
+vim.opt.number = true
+vim.opt.list = true
+vim.opt.listchars = "tab:|-,extends:⇢,precedes:⇠,trail:»"
+vim.opt.cmdheight = 0
+vim.opt.title = true
+vim.opt.pumheight = 20
+vim.opt.signcolumn = "yes"
+vim.opt.cursorline = true
+vim.opt.fileformats = "unix,dos,mac"
+vim.opt.mouse = "a"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.inccommand = "split"
+vim.opt.breakindent = true
 -- インクリメント時の挙動 デフォでは0や0xで始まる数字は8進，16進とみなされる
 -- 16進のみ許可し，他の数字は10進として扱う．
-opt.nf = "hex"
+vim.opt.nf = "hex"
 -- 画面端まで行かなくてもスクロールできる
-opt.scrolloff = 10
-opt.sidescrolloff = 5
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 5
 -- インデントで折りたたみ
-opt.foldmethod = "indent"
-opt.foldlevel = 99
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevel = 99
 -- clipboard
-opt.clipboard = "unnamed"
+vim.opt.clipboard = "unnamed"
 -- no backup
-opt.writebackup = false
-opt.swapfile = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
 -- disable conceal
-opt.conceallevel = 0
-opt.concealcursor = ""
+vim.opt.conceallevel = 0
+vim.opt.concealcursor = ""
 -- true color
-opt.termguicolors = true
+vim.opt.termguicolors = true
 if (not fn.has("gui_running")) and (g["&term"] ~= "screen" and g["&term"] ~= "tmux") then
 	g["&t_8f"] = "<Esc>[38;2;%lu;%lu;%lum"
 	g["&t_8b"] = "<Esc>[48;2;%lu;%lu;%lum"
