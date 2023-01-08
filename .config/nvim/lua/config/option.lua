238bc560500040298c4ae867d946c60c16d7a8e7
ton <tonton1517@gmail.com>
2023-01-09 02:48:45 +0900

    set floating window border style.

--- .config/nvim/lua/config/option.lua
+++ .config/nvim/lua/config/option.lua
@@ -49,8 +49,25 @@ if (not fn.has("gui_running")) and (g["&term"] ~= "screen" and g["&term"] ~= "tm
 	g["&t_8b"] = "<Esc>[48;2;%lu;%lu;%lum"
 end
 
+----------------------------------------------------------------------
+--                        lsp and diagnostic                        --
+----------------------------------------------------------------------
+--
+local border_style = "rounded"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+	border = border_style,
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+	border = border_style,
+})
+
 -- メッセージにsourceとcodeを表示する
 vim.diagnostic.config({
+	float = {
+		border = border_style,
+	},
 	virtual_text = {
 		format = function(diagnostic)
 			return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
