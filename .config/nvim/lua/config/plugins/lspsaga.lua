c3df2472d50cfd7b65f52ea10c4e3c736abcdae5
ton <tonton1517@gmail.com>
2023-01-17 01:34:57 +0900

    Revert "use native lsp function."
    This reverts commit b5a9d8e9774e35b5b3aac411d1f6cab94b85d0be.

--- .config/nvim/lua/config/plugins/lspsaga.lua
+++ .config/nvim/lua/config/plugins/lspsaga.lua
@@ -1,15 +1,18 @@
 return {
 	-- light-weight lsp plugin based on neovim built-in lsp with highly a performant UI.
 	"glepnir/lspsaga.nvim",
-	branch = "version_2.3",
 
 	config = function()
 		local keymap = vim.keymap.set
 		local opts = { silent = true }
+		keymap("n", ",e", "<cmd>Lspsaga show_line_diagnostics<cr>", opts)
+		keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+		keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
 
-		-- keymap("n", ",rn", "<cmd>Lspsaga rename<CR>", opts)
+		keymap("n", ",rn", "<cmd>Lspsaga rename<CR>", opts)
 		keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opts)
-		-- keymap({ "n", "v" }, ",ca", "<cmd>Lspsaga code_action<CR>", opts)
+		keymap({ "n", "v" }, ",ca", "<cmd>Lspsaga code_action<CR>", opts)
+		keymap("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
 		keymap("n", ",o", "<cmd>LSoutlineToggle<CR>", opts)
 
 		require("lspsaga").init_lsp_saga({
