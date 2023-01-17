2398a2e8947c672f7e31498e95d92cc973ba1c2b
ton <tonton1517@gmail.com>
2023-01-18 01:47:38 +0900

    use nvim-insx instead of nvim-autopairs.

--- .config/nvim/lua/config/plugins/nvim-autopairs.lua
+++
@@ -1,9 +0,0 @@
-return {
-	-- A super powerful autopair plugin for Neovim that supports multiple characters.
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-
-	config = function()
-		require("nvim-autopairs").setup({})
-	end,
-}
---
+++ .config/nvim/lua/config/plugins/nvim-insx.lua
@@ -0,0 +1,7 @@
+return {
+	"hrsh7th/nvim-insx",
+
+	config = function()
+		require("insx.preset.standard").setup()
+	end,
+}
