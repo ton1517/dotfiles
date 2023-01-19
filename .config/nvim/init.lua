b18a1669dbd82029e854611944941ab88cb20c9b
ton <tonton1517@gmail.com>
2023-01-20 01:50:50 +0900

    add statuscolumn setting.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,7 @@ require("config.disable-standard-plugins")
 require("config.keymap")
 require("config.option")
 require("config.lsp-diagnostic-option")
+require("config.statuscolumn")
 pcall(require, "config.local")
 
 require("config.lazy")
