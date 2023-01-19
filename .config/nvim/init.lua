35fbe40ccf597521ba8c000a446cef83d8389d98
ton <tonton1517@gmail.com>
2023-01-20 01:49:24 +0900

    separate option.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,7 @@
 require("config.disable-standard-plugins")
 require("config.keymap")
 require("config.option")
+require("config.lsp-diagnostic-option")
 pcall(require, "config.local")
 
 require("config.lazy")
---
+++ .config/nvim/lua/config/lsp-diagnostic-option.lua
@@ -0,0 +1,33 @@
+local border_style = "rounded"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+	border = border_style,
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+	border = border_style,
+})
+
+-- メッセージにsourceとcodeを表示する
+vim.diagnostic.config({
+	float = {
+		border = border_style,
+	},
+	virtual_text = {
+		format = function(diagnostic)
+			return string.format("%s (%s: %s)", diagnostic.message, diagnostic.source, diagnostic.code)
+		end,
+	},
+})
+
+-- show diagnostic sign with icon.
+local signs = {
+	Error = " ",
+	Warn = " ",
+	Hint = " ",
+	Info = " ",
+}
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
