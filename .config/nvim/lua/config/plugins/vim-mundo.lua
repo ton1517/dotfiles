36133452004c83a0e36171ded6a17686d8d8740f
ton <tonton1517@gmail.com>
2023-01-21 04:29:16 +0900

    delete unused plugins.

--- .config/nvim/lua/config/plugins/vim-mundo.lua
+++
@@ -1,9 +0,0 @@
-return {
-	-- A Vim plugin to visualizes the Vim undo tree.
-	"simnalamburt/vim-mundo",
-	cmd = "MundoToggle",
-
-	init = function()
-		vim.keymap.set("n", ",mu", ":MundoToggle<CR>")
-	end,
-}
