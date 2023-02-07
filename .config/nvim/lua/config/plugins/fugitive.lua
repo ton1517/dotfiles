757b9f9e8d548653d76b1dab14f204aea6be5ab6
ton <tonton1517@gmail.com>
2023-02-07 19:54:38 +0900

    fix config to init.

--- .config/nvim/lua/config/plugins/fugitive.lua
+++ .config/nvim/lua/config/plugins/fugitive.lua
@@ -7,7 +7,7 @@ return {
 	},
 	cmd = { "Git", "GBrowse" },
 
-	config = function()
+	init = function()
 		local keymap = vim.keymap.set
 		keymap("n", "<C-g><C-b>", "<cmd>Git blame<cr>")
 		keymap("n", "<C-g><C-o>", "<cmd>GBrowse<cr>")
