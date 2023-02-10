6b4ae7de30ebb2a9faefb561d155923299f78e6d
ton <tonton1517@gmail.com>
2023-02-10 17:54:29 +0900

    use native lsp function.

--- .config/nvim/lua/config/plugins/lspsaga.lua
+++ .config/nvim/lua/config/plugins/lspsaga.lua
@@ -6,23 +6,11 @@ return {
 	config = function()
 		local keymap = vim.keymap.set
 		local opts = { silent = true }
-		keymap("n", ",e", "<cmd>Lspsaga show_line_diagnostics<cr>", opts)
-		keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
-		keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-		keymap("n", "[E", function()
-			require("lspsaga.diagnostic").goto_prev({ severity = vim.diagnostic.severity.ERROR })
-		end)
-		keymap("n", "]E", function()
-			require("lspsaga.diagnostic").goto_next({ severity = vim.diagnostic.severity.ERROR })
-		end)
-
 		keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts)
 		keymap("n", "gD", "<cmd>Lspsaga peek_definition<CR>", opts)
 
-		keymap("n", ",rn", "<cmd>Lspsaga rename<CR>", opts)
 		keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opts)
 		keymap({ "n", "v" }, ",ca", "<cmd>Lspsaga code_action<CR>", opts)
-		keymap("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
 		keymap("n", ",o", "<cmd>Lspsaga outline<CR>", opts)
 
 		require("lspsaga").setup({
--- .config/nvim/lua/config/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/config/plugins/nvim-lspconfig.lua
@@ -11,9 +11,23 @@ return {
 	config = function()
 		local keymap = vim.keymap.set
 		local opt = { noremap = true, silent = true }
+
+		keymap("n", ",e", vim.diagnostic.open_float, opt)
+		keymap("n", "[e", vim.diagnostic.goto_prev, opt)
+		keymap("n", "]e", vim.diagnostic.goto_next, opt)
+		keymap("n", "[E", function()
+			vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+		end)
+		keymap("n", "]E", function()
+			vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+		end)
+
 		keymap("n", "gy", vim.lsp.buf.type_definition, opt)
 		keymap("n", "gr", vim.lsp.buf.references, opt)
 		keymap("n", "gi", vim.lsp.buf.implementation, opt)
+
+		keymap("n", "K", vim.lsp.buf.hover, opt)
+		keymap("n", ",rn", vim.lsp.buf.rename, opt)
 
 		keymap("n", ",wa", vim.lsp.buf.add_workspace_folder, opt)
 		keymap("n", ",wr", vim.lsp.buf.remove_workspace_folder, opt)
