dae1c96afa448b1f5979d1c03d962673e5b90064
ton <tonton1517@gmail.com>
2023-06-29 02:03:42 +0900

    update mappings.

--- .config/nvim/lua/config/plugins/flash.lua
+++ .config/nvim/lua/config/plugins/flash.lua
@@ -5,36 +5,12 @@ return {
 	opts = {},
 	keys = {
 		{
-			"s",
+			"<space>",
 			mode = { "n", "x", "o" },
 			function()
 				require("flash").jump()
 			end,
 			desc = "Flash",
-		},
-		{
-			"S",
-			mode = { "n", "o", "x" },
-			function()
-				require("flash").treesitter()
-			end,
-			desc = "Flash Treesitter",
-		},
-		{
-			"r",
-			mode = "o",
-			function()
-				require("flash").remote()
-			end,
-			desc = "Remote Flash",
-		},
-		{
-			"R",
-			mode = { "n", "o", "x" },
-			function()
-				require("flash").treesitter_search()
-			end,
-			desc = "Treesitter Search",
 		},
 	},
 }
