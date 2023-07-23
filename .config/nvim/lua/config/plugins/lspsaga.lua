cb8e3ed1811524ad8865d91c6fa77cfe8e4d568a
ton <tonton1517@gmail.com>
2023-07-23 17:26:32 +0900

    update lspsaga.

--- .config/nvim/lua/config/plugins/lspsaga.lua
+++ .config/nvim/lua/config/plugins/lspsaga.lua
@@ -1,6 +1,6 @@
 return {
 	-- light-weight lsp plugin based on neovim built-in lsp with highly a performant UI.
-	"glepnir/lspsaga.nvim",
+	"nvimdev/lspsaga.nvim",
 	event = { "LspAttach" },
 	dependencies = {
 		{ "nvim-tree/nvim-web-devicons" },
@@ -13,28 +13,23 @@ return {
 		keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts)
 		keymap("n", "gD", "<cmd>Lspsaga peek_definition<CR>", opts)
 
-		keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opts)
+		keymap("n", "gh", "<cmd>Lspsaga finder<CR>", opts)
 		keymap({ "n", "v" }, ",ca", "<cmd>Lspsaga code_action<CR>", opts)
 		keymap("n", ",o", "<cmd>Lspsaga outline<CR>", opts)
 
 		require("lspsaga").setup({
-			request_timeout = 15000,
 			symbol_in_winbar = {
 				enable = false,
 			},
 			finder = {
-				edit = { "o", "<CR>" },
-				vsplit = "v",
-				split = "s",
-				tabe = "t",
-				quit = { "q", "<ESC>" },
+				keys = {
+					vsplit = "v",
+					split = "s",
+				},
 			},
 			code_action = {
 				num_shortcut = true,
-				keys = {
-					quit = { "q", "<ESC>" },
-					exec = "<CR>",
-				},
+				show_server_name = true,
 			},
 			rename = {
 				quit = "<ESC>",
