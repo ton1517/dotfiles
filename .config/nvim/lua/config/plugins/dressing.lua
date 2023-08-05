eb99c74601ecb2498e62f83c0315ad03f2994993
ton <tonton1517@gmail.com>
2023-08-05 15:36:56 +0900

    update dressing.lua.

--- .config/nvim/lua/config/plugins/dressing.lua
+++ .config/nvim/lua/config/plugins/dressing.lua
@@ -12,7 +12,7 @@ return {
 				default_prompt = "Input:",
 
 				-- Can be 'left', 'right', or 'center'
-				prompt_align = "left",
+				title_pos = "left",
 
 				-- When true, <Esc> will close the modal
 				insert_only = true,
@@ -21,7 +21,6 @@ return {
 				start_in_insert = true,
 
 				-- These are passed to nvim_open_win
-				anchor = "SW",
 				border = "rounded",
 				-- 'editor' and 'win' will default to being centered
 				relative = "cursor",
@@ -40,6 +39,11 @@ return {
 					winblend = 10,
 					-- Disable line wrapping
 					wrap = false,
+					-- Indicator for when text exceeds window
+					list = true,
+					listchars = "precedes:…,extends:…",
+					-- Increase this for more context when text scrolls off the window
+					sidescrolloff = 0,
 				},
 
 				-- Set to `false` to disable
@@ -70,7 +74,7 @@ return {
 				enabled = true,
 
 				-- Priority list of preferred vim.select implementations
-				backend = { "nui", "telescope", "fzf_lua", "fzf", "builtin" },
+				backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
 
 				-- Trim trailing `:` from prompt
 				trim_prompt = true,
@@ -88,12 +92,12 @@ return {
 					},
 				},
 
-				-- Options for fzf_lua selector
+				-- Options for fzf-lua
 				fzf_lua = {
-					winopts = {
-						width = 0.5,
-						height = 0.4,
-					},
+					-- winopts = {
+					--   height = 0.5,
+					--   width = 0.5,
+					-- },
 				},
 
 				-- Options for nui Menu
@@ -120,7 +124,6 @@ return {
 				-- Options for built-in selector
 				builtin = {
 					-- These are passed to nvim_open_win
-					anchor = "NW",
 					border = "rounded",
 					-- 'editor' and 'win' will default to being centered
 					relative = "editor",
@@ -129,6 +132,8 @@ return {
 					win_options = {
 						-- Window transparency (0-100)
 						winblend = 10,
+						cursorline = true,
+						cursorlineopt = "both",
 					},
 
 					-- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
