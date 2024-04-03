bd71fd16538c7410b09302674b94425f17029c2b
ton <tonton1517@gmail.com>
2024-04-03 13:50:08 +0900

    add mason-null-ls.

---
+++ .config/nvim/lua/config/plugins/mason-null-ls.lua
@@ -0,0 +1,11 @@
+return {
+	"jay-babu/mason-null-ls.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"nvimtools/none-ls.nvim",
+	},
+	opts = {
+		handlers = {},
+		automatic_installation = true,
+	},
+}
