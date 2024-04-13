f26f9570d7d0593e6abbfa546b627096e5f7f19a
ton <tonton1517@gmail.com>
2024-04-13 14:45:37 +0900

    update config for nvim-notify .

--- .config/nvim/lua/config/plugins/nvim-notify.lua
+++ .config/nvim/lua/config/plugins/nvim-notify.lua
@@ -1,7 +1,29 @@
 return {
 	-- A fancy, configurable, notification manager for NeoVim
 	"rcarriga/nvim-notify",
+	keys = {
+		{
+			"<leader>un",
+			function()
+				require("notify").dismiss({ silent = true, pending = true })
+			end,
+			desc = "Dismiss all Notifications",
+		},
+	},
 
+	opts = {
+		stages = "static",
+		timeout = 3000,
+		max_height = function()
+			return math.floor(vim.o.lines * 0.75)
+		end,
+		max_width = function()
+			return math.floor(vim.o.columns * 0.75)
+		end,
+		on_open = function(win)
+			vim.api.nvim_win_set_config(win, { zindex = 100 })
+		end,
+	},
 	config = function()
 		vim.notify = require("notify")
 	end,
