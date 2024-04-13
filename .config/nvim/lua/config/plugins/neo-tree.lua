673c87ee7590c17cca13736651055449c8700560
ton <tonton1517@gmail.com>
2024-04-13 15:35:28 +0900

    update neo-tree.

--- .config/nvim/lua/config/plugins/neo-tree.lua
+++ .config/nvim/lua/config/plugins/neo-tree.lua
@@ -93,7 +93,9 @@ return {
 						".git",
 					},
 				},
-				follow_current_file = true,
+				follow_current_file = {
+					enabled = true,
+				},
 				hijack_netrw_behavior = "open_default",
 				use_libuv_file_watcher = true,
 			},
