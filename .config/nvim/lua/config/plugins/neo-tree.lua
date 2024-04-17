3dce8fb286972ecc436adde2cf953bd646d6d069
ton <tonton1517@gmail.com>
2024-04-17 14:59:03 +0900

    update neo-tree config.

--- .config/nvim/lua/config/plugins/neo-tree.lua
+++ .config/nvim/lua/config/plugins/neo-tree.lua
@@ -21,9 +21,14 @@ return {
     ]])
 		require("neo-tree").setup({
 			close_if_last_window = true,
+			popup_border_style = "rounded",
 			enable_git_status = true,
 			enable_diagnostics = true,
 			default_component_configs = {
+				indent = {
+					indent_size = 1,
+					padding = 0,
+				},
 				git_status = {
 					symbols = {
 						deleted = "✖",
