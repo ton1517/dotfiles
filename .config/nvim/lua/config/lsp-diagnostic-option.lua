b59a76fd2dbbbfbeed3a20b28c59629dad560c7b
ton <tonton1517@gmail.com>
2024-04-17 14:46:10 +0900

    fix api for diagnostic config.

--- .config/nvim/lua/config/lsp-diagnostic-option.lua
+++ .config/nvim/lua/config/lsp-diagnostic-option.lua
@@ -22,13 +22,13 @@ vim.diagnostic.config({
 })
 
 -- show diagnostic sign with icon.
-local signs = {
-	Error = " ",
-	Warn = " ",
-	Hint = " ",
-	Info = " ",
-}
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-end
+vim.diagnostic.config({
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = " ",
+			[vim.diagnostic.severity.WARN] = " ",
+			[vim.diagnostic.severity.HINT] = " ",
+			[vim.diagnostic.severity.INFO] = " ",
+		},
+	},
+})
