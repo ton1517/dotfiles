fc86e6dd1106aea587d0f43a5e2b6459c866715e
ton <tonton1517@gmail.com>
2024-04-18 23:36:33 +0900

    fix none-ls config.

--- .config/nvim/lua/config/plugins/null-ls.lua
+++ .config/nvim/lua/config/plugins/null-ls.lua
@@ -42,14 +42,26 @@ return {
 				null_ls.builtins.diagnostics.golangci_lint,
 				null_ls.builtins.diagnostics.codespell,
 				require("none-ls.diagnostics.flake8"),
-				require("none-ls.diagnostics.eslint_d"),
+				require("none-ls.diagnostics.eslint_d").with({
+					condition = function(utils)
+						return utils.has_file({ ".eslintrc.json", ".eslintrc.js", ".eslint.config.js" })
+					end,
+				}),
 
 				-- Formatting
 				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.dart_format,
 				null_ls.builtins.formatting.goimports,
-				require("none-ls.formatting.jq"),
-				require("none-ls.formatting.eslint_d"),
+				require("none-ls.formatting.jq").with({
+					condition = function(utils)
+						return utils.has_file({ "biome.json", "biome.jsonc" }) == false
+					end,
+				}),
+				require("none-ls.formatting.eslint_d").with({
+					condition = function(utils)
+						return utils.has_file({ ".eslintrc.json", ".eslintrc.js", ".eslint.config.js" })
+					end,
+				}),
 				null_ls.builtins.formatting.prettierd.with({
 					condition = function(utils)
 						return utils.has_file({ ".prettierrc", ".prettierrc.js" })
