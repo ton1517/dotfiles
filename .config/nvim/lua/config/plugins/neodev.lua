71286ee66a9fb79b7450be78e71fe41182a24073
ton <tonton1517@gmail.com>
2024-04-27 12:15:02 +0900

    update neotest.

--- .config/nvim/lua/config/plugins/neodev.lua
+++ .config/nvim/lua/config/plugins/neodev.lua
@@ -2,4 +2,10 @@ return {
 	-- Dev init for init.lua and plugin development with full signature help, docs and completion for the nvim lua API.
 	"folke/neodev.nvim",
 	lazy = true,
+
+	config = function()
+		require("neodev").setup({
+			library = { plugins = { "neotest" }, types = true },
+		})
+	end,
 }
--- .config/nvim/lua/config/plugins/neotest.lua
+++ .config/nvim/lua/config/plugins/neotest.lua
@@ -2,7 +2,9 @@ return {
 	-- An extensible framework for interacting with tests within NeoVim.
 	"nvim-neotest/neotest",
 	dependencies = {
+		"nvim-neotest/nvim-nio",
 		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-neotest/neotest-go",
 		"haydenmeade/neotest-jest",
